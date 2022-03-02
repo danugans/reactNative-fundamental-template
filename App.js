5e2f6a03ed33d21dc6fde63e43be6b995bc74390
danugans <mas.danu29@gmail.com>
2022-03-02 08:39:47 +0700

    update

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View } from "react-native";
 
-// Import Component
+// Import Component here
 
 export default function App() {
   return (
